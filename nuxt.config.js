c4b79d8c530dad7e10f629b3b5e381654c8f7ebe
suxi <sx_syj@163.com>
2021-09-24 17:25:48 +0800

    导入页面

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+export default {
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.splice(0)
+      routes.push(
+        ...[
+          {
+            path: '/',
+            component: resolve(__dirname, 'pages/layout/'),
+            children: [
+              {
+                name: 'home',
+                path: '',
+                component: resolve(__dirname, 'pages/home/')
+              },
+              {
+                name: 'article',
+                path: '/article/:slug',
+                component: resolve(__dirname, 'pages/article/')
+              },
+              {
+                name: 'editor',
+                path: '/editor/:slug?',
+                component: resolve(__dirname, 'pages/editor/')
+              },
+              {
+                name: 'login',
+                path: '/login',
+                component: resolve(__dirname, 'pages/login/')
+              },
+              {
+                name: 'register',
+                path: '/register',
+                component: resolve(__dirname, 'pages/login/')
+              },
+              {
+                name: 'settings',
+                path: '/settings',
+                component: resolve(__dirname, 'pages/settings/')
+              },
+              {
+                name: 'profile',
+                path: '/profile/:username',
+                component: resolve(__dirname, 'pages/profile/')
+              }
+            ]
+          }
+        ]
+      )
+    }
+  }
+}
